597708fd816c22aa3960d5630080662095f0fe04
Fate <97143545+Fvted@users.noreply.github.com>
2022-04-19 15:11:15 -0700

    Added a "decrypt

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ def encrypt(string):
     encrypted = [hex(ord(c) ^ 5)[2:] for c in string]
     return "".join(encrypted)
 
+def decrypt(string):
+    decrypted = []
+    for i in range(0, len(string), 2):
+        current = bytearray.fromhex(string[i:i+2]).decode()
+        current = 5 ^ ord(current)
+        decrypted.append(chr(current))
+    return "".join(decrypted)
+
 if __name__ == "__main__":
-    pre = input("To encrypt: ")
-    print("Encrypted:", encrypt(pre))
+    encrypted = encrypt(input("To encrypt: "))
+    print("Encrypted:", encrypted)
+    print("Decrypted:", decrypt(encrypted))
